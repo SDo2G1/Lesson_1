8565a289232e313cce6fcdbcd96ea71d7272dd8e
Stanislav <SDo2G1@users.noreply.github.com>
2024-04-22 17:30:48 +0300

    TASK

---
+++ Lesson_1.py
@@ -0,0 +1,25 @@
+
+
+
+v_int = 6.155
+print(v_int)
+
+v_string = '?'
+print(v_string)
+
+v_float = 3
+print(v_float)
+
+v_string = '='
+
+print(v_string)
+
+v_int2 = v_int % v_float
+
+#result = v_int1 * v_int2
+
+print(v_int2)
+
+v_string = 'я твою маму ебал нуб ебаный'
+
+print(v_string)
