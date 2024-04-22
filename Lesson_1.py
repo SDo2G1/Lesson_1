3cb38eb97008e5fa404b808a9d526249f26f3158
Stanislav <SDo2G1@users.noreply.github.com>
2024-04-22 19:10:48 +0300

    Float to int with round

--- Lesson_1.py
+++ Lesson_1.py
@@ -1,39 +1,7 @@
 
+num = 7.51
 
+value = round(num)
 
-v_int = 6.1567868
-
-print(v_int)
-
-v_string = '?'
-print(v_string)
-
-v_float = 3
-print(v_float)
-
-v_string = '='
-
-print(v_string)
-
-v_int2 = v_int % v_float
-
-#result = v_int1 * v_int2
-
-print(v_int2)
-
-v_string = 'первый урок усвоен'
-
-print(v_string)
-
-num = 9.3
-
-
-print('type:',
-      type(num).__name__)
-
-
-num = int(num)
-
-
-print('converted value:', num,
-      ', type:', type(num).__name__)
+print( 'type : ',type(value).__name__)
+print("the result using round : ",value)
