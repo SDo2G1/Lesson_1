16cab5b910e8e67f76875a7c87b170d4641c7f5b
Stanislav <SDo2G1@users.noreply.github.com>
2024-04-23 20:35:01 +0300

    Lesson 2

---
+++ Lesson_2.py
@@ -0,0 +1,22 @@
+#Урок второй...словари
+
+
+
+list1 = [6547, 567, 89, 'come as you are']
+
+print(list1)
+#цифра 2 значит порядковый номер в листе
+print(list1[2])
+
+
+
+dic = dict()
+print(dic)
+
+
+dic["first"] = 1
+#dic["second"] = 12345
+
+dic["first"] = 'hello!'
+
+print(dic)
